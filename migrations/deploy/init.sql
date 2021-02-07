ba092e6d84e2167d8c7b52e31f5b546b263a9eb2
LoreneMeslin <lorene.meslin@gmail.com>
2021-02-07 16:26:23 +0100

    ajout de commentaire sur les lignes pouvant devenir des tables à part plus tard

--- migrations/deploy/init.sql
+++ migrations/deploy/init.sql
@@ -8,11 +8,11 @@ CREATE DOMAIN posint AS int CHECK (value > 0);
 CREATE TABLE book (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title text NOT NULL,
-    author text NOT NULL,
-    "edition" text NOT NULL,
+    author text NOT NULL, -- possible futur table
+    "edition" text NOT NULL, -- possible futur table
     isbn text NOT NULL,
     number_pages posint NOT NULL,
-    tag text NOT NULL,
+    tag text NOT NULL, -- possible futur table
     note posint NOT NULL
 );
 
